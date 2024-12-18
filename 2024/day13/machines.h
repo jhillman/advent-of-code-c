9c67a7d9dc7a472175ecceae9dadbd54e0fba935
Jeff Hillman <hillmanapps@gmail.com>
2024-12-18 15:16:49 -0700

    Day 13

--- 2024/day13/machines.h
+++ 2024/day13/machines.h
@@ -30,15 +30,12 @@ long long totalCost(char *input, long long offset) {
 
             // https://en.wikipedia.org/wiki/Cramer%27s_rule
             long long determinant = machine.a.x * machine.b.y - machine.a.y * machine.b.x;
+            long long aPresses = (machine.prize.x * machine.b.y - machine.prize.y * machine.b.x) / determinant;
+            long long bPresses = (machine.prize.y * machine.a.x - machine.prize.x * machine.a.y) / determinant;
 
-            if (determinant != 0) {
-                long long aPresses = (machine.prize.x * machine.b.y - machine.prize.y * machine.b.x) / determinant;
-                long long bPresses = (machine.prize.y * machine.a.x - machine.prize.x * machine.a.y) / determinant;
-
-                if (machine.a.x * aPresses + machine.b.x * bPresses == machine.prize.x && 
-                    machine.a.y * aPresses + machine.b.y * bPresses == machine.prize.y) {
-                    cost += 3LL * aPresses + bPresses;
-                }
+            if (machine.a.x * aPresses + machine.b.x * bPresses == machine.prize.x && 
+                machine.a.y * aPresses + machine.b.y * bPresses == machine.prize.y) {
+                cost += 3LL * aPresses + bPresses;
             }
         }
 
